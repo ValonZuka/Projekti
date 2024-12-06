624167ec6cc3d98c19a3865f5ed6462c4f234ac0
ValonZuka <147207158+ValonZuka@users.noreply.github.com>
2024-12-06 23:11:41 +0100

    ~ added sideMenu bar ~ ~ icon images ~

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    const showmenu = document.querySelector('.sideBar');
+    showmenu.style.display = 'flex';
+}
+function hideMenu(){
+    const showmenu = document.querySelector('.sideBar');
+    showmenu.style.display = 'none'  ;  
+}
