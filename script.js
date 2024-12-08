5961a490f9bd54353238274a9526c8d90796738b
ValonZuka <147207158+ValonZuka@users.noreply.github.com>
2024-12-08 01:42:32 +0100

    .

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function showMenu(){
-    const showmenu = document.querySelector('.sideBar');
-    showmenu.style.display = 'flex';
+function toggleMenu(){
+const sideBar = document.querySelector('.sideBar');
+const menuIcon = document.getElementById('menuIcon');
+const navlink = document.querySelectorAll('.nav-link a')
+
+if(sideBar.classList.contains('active')){
+    sideBar.classList.remove('active');
+    sideBar.classList.remove('visible');
+    menuIcon.src = 'icon.menu/open.png';
+
+   
+
+}else{
+    sideBar.classList.add ('active');
+    sideBar.classList.add('visible');
+    menuIcon.src = 'icon.menu/close.png';
+
+    
 }
-function hideMenu(){
-    const showmenu = document.querySelector('.sideBar');
-    showmenu.style.display = 'none'  ;  
+
 }
+
